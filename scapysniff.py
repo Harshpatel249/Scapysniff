56a7de36e107a995c6f454db1b7549ba0828168b
= <=>
2020-11-18 22:11:10 +0530

    Made the function useable

--- scapysniff.py
+++ scapysniff.py
@@ -6,11 +6,10 @@ IPAddr = socket.gethostbyname(hostname)
 
 def sniffp(pkt):
     if(pkt[IP].src == IPAddr):
-        print("Packet type = Response, " + "Packet Size = " + str(len(pkt)) + " Sequence number = " + str(pkt.seq))
+        return ("Packet type = Response, " + "Packet Size = " + str(len(pkt)) + " Sequence number = " + str(pkt.seq))
     else:
-        print("Packet type = Request, " + "Packet Size = " + str(len(pkt)) + " Sequence number = " + str(pkt.seq))
+        return ("Packet type = Request, " + "Packet Size = " + str(len(pkt)) + " Sequence number = " + str(pkt.seq))
 
 if __name__ == "__main__":
-    sniff(prn = sniffp, filter = "icmp")
-
-
+    packet = sniff(prn = sniffp, filter = "icmp",count = 5)
+    print(packet)
